e7a1f7d49410d1cbe9d2b7bc2b7c1ffe127d6e01
Patric-k-k <beepbooppk@gmail.com>
2025-07-31 23:53:07 -0400

    did some things

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Website hosting info pages for Project: StormCore
 
 ## Setup
 In order to setup, you must first run `/internal/create_structure.php` whilst `internal/setupkillswitch` doesn't contain `1`.
+
+## Running
+Run the command `php -S 0.0.0.0:80 router.php` while in the directory.
--- internal/create_structure.php
+++ internal/create_structure.php
@@ -4,4 +4,9 @@ $ks = file_get_contents("setupkillswitch");
 if ($ks != "1") {
     mkdir("../data/");
     file_put_contents("../data/codes.dat","");
+    file_put_contents("../data/di.dat","");
+    file_put_contents("../data/hr.dat","");
+    file_put_contents("../data/si.dat","");
+    file_put_contents("../data/staff.dat","");
+    file_put_contents("../data/superviser.dat","");
 }
